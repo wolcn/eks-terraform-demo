bca57419da7a816fc1bd5ca0438b2fd7c544e9d1
john <johann.whisk@gmail.com>
2025-07-15 12:00:50 +0200

    Changed to Fairwinds Helm chart for VPA

---
+++ files/README.md
@@ -0,0 +1,1 @@
+Folder that I can put files that are not part of the Terraform package but useful for what I'm doing anyway
--- vpa.tf
+++ vpa.tf
@@ -30,7 +30,7 @@ resource "helm_release" "vpa" {
 
   name       = "vpa"
   namespace  = kubernetes_namespace.vpa.metadata[0].name
-  chart      = "vertical-pod-autoscaler"
-  repository = "https://cowboysysop.github.io/charts/"
-  version    = "10.2.1"
+  chart      = "vpa"
+  repository = "https://charts.fairwinds.com/stable"
+  version    = "4.7.2"
 }
